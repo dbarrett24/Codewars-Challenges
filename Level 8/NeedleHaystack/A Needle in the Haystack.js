a5e05b16159aef2b1f940cb8cf68a17b890c4605
dbarrett24 <dbarrett24@gmail.com>
2017-10-25 10:52:30 -0600

    needle in a haystack

--- Level 8/NeedleHaystack/A Needle in the Haystack.js
+++ Level 8/NeedleHaystack/A Needle in the Haystack.js
@@ -1,3 +1,7 @@
+//input or tests
+let haystack = ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'];
+
+//Solution
 function findNeedle(haystack) {
     for(var i = 0; i < haystack.length; i++){
       if(haystack[i]=== "needle"){
@@ -5,3 +9,12 @@ function findNeedle(haystack) {
       }
     }
 }
+
+//Alternative Solution:
+function findNeedle2(haystack){
+  return "found the needle at position " + haystack.indexOf('needle');
+}
+
+//Run the functions:
+findNeedle(haystack);
+findNeedle2(haystack);
